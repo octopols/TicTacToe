034f4e29a24890fdb7c3b92366291bd654b57474
= <=>
2021-11-30 19:38:10 -0800

    Initial Commit

---
+++ app/src/main/java/com/hirnaymay/TicTacToe/MainActivity.java
@@ -0,0 +1,100 @@
+package com.hirnaymay.TicTacToe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+    int activePlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winPositions = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}
+    };
+
+    public void playerTap(View view) {
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if(gameState[tappedImage] == 2 && gameActive) {
+            gameState[tappedImage] = activePlayer;
+            if(activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText(R.string.o_turn);
+            }
+            else {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText(R.string.x_turn);
+            }
+        }
+        if(!gameActive) {
+            gameReset(view);
+        }
+        for (int[] winPositions: winPositions) {
+            if (gameState[winPositions[0]] == gameState[winPositions[1]] &&
+                    gameState[winPositions[1]] == gameState[winPositions[2]] &&
+                    gameState[winPositions[0]] != 2) {
+                String winnerStr;
+                gameActive = false;
+                if (gameState[winPositions[0]] == 0) {
+                    winnerStr = "X won";
+                }
+                else {
+                    winnerStr = "O won";
+                }
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+
+            }
+        }
+        boolean emptySquare = false;
+        for (int squareState:gameState) {
+            if(squareState == 2) {
+                emptySquare = true;
+                break;
+            }
+        }
+        if(!emptySquare && gameActive) {
+            gameActive = false;
+            String winnerStr = "Game: Tie";
+            TextView status = findViewById(R.id.status);
+            status.setText(winnerStr);
+        }
+
+    }
+    public void gameReset(View view) {
+        gameActive = true;
+        activePlayer = 0;
+        Arrays.fill(gameState, 2);
+        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+}
